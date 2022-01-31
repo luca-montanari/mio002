a616d47449bba9cd9728c973db017c65aafa90f2
Luca Montanari <luca.montanari@libero.it>
2022-01-31 07:09:59 +0100

    casa

--- src/app/db/models/docs/doc.ts
+++ src/app/db/models/docs/doc.ts
@@ -1,6 +1,8 @@
+import { DateTime } from "luxon";
+
 export interface Doc {
     code: string;
     description: string;
     category?: string;
-    timestampServerCreated: 
+    timestampServerCreated: DateTime;
 }
